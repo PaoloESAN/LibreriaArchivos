bef2f1e03dcbd49919fbd7c5294c3dd561819bb1
Paolo <paoloejemplo124@gmail.com>
2025-05-04 19:58:33 -0500

    Add: Devolver lista del archivo leido

--- src/Archivos/Archivo.java
+++ src/Archivos/Archivo.java
@@ -3,6 +3,8 @@ package Archivos;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Archivo {
     
@@ -17,5 +19,26 @@ public class Archivo {
             System.out.println("error: " + e.getMessage());
         }
         return respuesta;
+    }
+    
+        public static List<String[]> leerLista(String ruta){
+        List<String[]> lista = new ArrayList<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(ruta))) {
+            String linea;
+            while ((linea = br.readLine()) != null) {
+                String[] partes = linea.trim().split("\\s+"); 
+                //.trim elimina los espacios al inicio y al final
+                // .split divide el texto
+                // \\s+ : en java para poner "\" debes ponerlo 2 veces, entonces seria: \s+
+                // \s significa cualquier caracter en blanco
+                // + significa uno o más
+                // \s+ entonces significa uno o mas caracteres en blanco
+                lista.add(partes);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return lista;
     }
 }
