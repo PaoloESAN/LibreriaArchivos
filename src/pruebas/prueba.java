371eb57863f7c75262e4f0cc607b5788a3a43498
PaoloESAN <186249117+PaoloESAN@users.noreply.github.com>
2025-05-04 19:58:33 -0500

    Add: Devolver lista del archivo leido

--- src/pruebas/prueba.java
+++ src/pruebas/prueba.java
@@ -1,11 +1,16 @@
 package pruebas;
 
 import Archivos.Archivo;
+import java.util.List;
 
 public class prueba {
     
     public static void main(String[] args) {
-        System.out.println(Archivo.leer("Usuarios.TXT"));
+        List<String[]> lista = Archivo.leerLista("Usuarios.TXT");
+        for(String[] fila : lista){
+            System.out.println(fila[10]);
+        }
+        System.out.println("");
         PruebaForm pb = new PruebaForm();
         pb.setVisible(true);
     }
